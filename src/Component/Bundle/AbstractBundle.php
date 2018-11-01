a873c90e5e0d24630397b601360eae9dd5edd0e9
arif <arifsetiantoo@gmail.com>
2018-11-01 14:14:01 +0700

    Fix doctrine mapping driver

--- src/Component/Bundle/AbstractBundle.php
+++ src/Component/Bundle/AbstractBundle.php
@@ -35,7 +35,7 @@ abstract class AbstractBundle extends Bundle
      */
     protected function getMappingDriver(): array
     {
-        return [__DIR__ . sprintf('/%s/Resources/config/doctrine', $this->getBundleName()) => $this->getModelNamespace()];
+        return [sprintf('/%s/Resources/config/doctrine', $this->getBundleName()) => $this->getModelNamespace()];
     }
 
     /**
