8006b74228387dd3a07746b3399bf16d36243263
arif <arifsetiantoo@gmail.com>
2018-11-03 15:46:26 +0700

    Add enum package

---
+++ src/Component/Shared/AbstractEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaby\Component\Shared;
+
+use MabeEnum\Enum;
+
+/**
+ * @author  Arif Setianto <arifsetiantoo@gmail.com>
+ */
+abstract class AbstractEnum extends Enum
+{
+}
