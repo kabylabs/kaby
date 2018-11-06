94e1841573af3cb5671c8f364fdd1907e5ace917
arif <arifsetiantoo@gmail.com>
2018-11-06 10:27:10 +0700

    Fix abstract query

--- src/Component/Message/AbstractQuery.php
+++ src/Component/Message/AbstractQuery.php
@@ -9,32 +9,25 @@ namespace Kaby\Component\Message;
  */
 abstract class AbstractQuery extends AbstractMessage
 {
-    /**
-     * @var int
-     */
     const CURRENT_PAGE = 1;
-
-    /**
-     * @var int
-     */
     const MAX_PER_PAGE = 50;
 
     /**
      * @var int
      */
-    private $page = self::CURRENT_PAGE;
+    private $page;
 
     /**
      * @var int
      */
-    private $limit = self::MAX_PER_PAGE;
+    private $limit;
 
     /**
      * @return int
      */
     public function getPage(): int
     {
-        return $this->page;
+        return $this->page ?: self::CURRENT_PAGE;
     }
 
     /**
@@ -42,6 +35,6 @@ abstract class AbstractQuery extends AbstractMessage
      */
     public function getLimit(): int
     {
-        return $this->limit;
+        return $this->limit ?: self::MAX_PER_PAGE;
     }
 }
