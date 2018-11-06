42886ca539b2ed5e3aeb2be606fb5a75b3851f7c
arif <arifsetiantoo@gmail.com>
2018-11-06 13:45:04 +0700

    Add container in abstract message

--- src/Component/Message/AbstractMessage.php
+++ src/Component/Message/AbstractMessage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kaby\Component\Message;
 
 use Kaby\Component\Http\Request\RequestContainerTrait;
+use Psr\Container\ContainerInterface;
 
 /**
  * @author  Arif Setianto <arifsetiantoo@gmail.com>
@@ -12,6 +13,11 @@ use Kaby\Component\Http\Request\RequestContainerTrait;
 abstract class AbstractMessage
 {
     use PayloadTrait, RequestContainerTrait;
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
 
     /**
      * AbstractMessage constructor.
@@ -23,5 +29,13 @@ abstract class AbstractMessage
     final public function __construct(array $payload = [])
     {
         $this->setPayload(array_merge($payload, $this->getRequestAll()));
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    protected function getContainer(): ContainerInterface
+    {
+        return $this->container;
     }
 }
